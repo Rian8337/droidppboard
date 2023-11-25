2909e4b6935f3db2eb2f8750f6455db0b8b0cffb
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2023-11-25 18:51:45 +0800

    Display forced beatmap statistics in calculation result

--- client/src/components/calculate/ParameterCalculationResult.tsx
+++ client/src/components/calculate/ParameterCalculationResult.tsx
@@ -8,6 +8,17 @@ export default function ParameterCalculationResult(props: {
     estimated: boolean;
 }) {
     const { beatmap, params } = props;
+    const modifiedStats: string[] = [];
+
+    if (params.forcecs !== undefined && !Number.isNaN(params.forcecs)) {
+        modifiedStats.push(`CS: ${params.forcecs.toFixed(2)}`);
+    }
+    if (params.forcear !== undefined && !Number.isNaN(params.forcear)) {
+        modifiedStats.push(`AR: ${params.forcear.toFixed(2)}`);
+    }
+    if (params.forceod !== undefined && !Number.isNaN(params.forceod)) {
+        modifiedStats.push(`OD: ${params.forceod.toFixed(2)}`);
+    }
 
     return (
         <table className="calculation-result">
@@ -88,31 +99,8 @@ export default function ParameterCalculationResult(props: {
                     </td>
                 </tr>
                 <tr>
-                    <th>Force CS</th>
-                    <td>
-                        {params.forcecs
-                            ? `CS: ${Math.max(0, Math.min(params.forcecs, 11))}`
-                            : "None"}
-                    </td>
-                </tr>
-                <tr>
-                    <th>Force AR</th>
-                    <td>
-                        {params.forcear
-                            ? `AR: ${Math.max(
-                                  0,
-                                  Math.min(params.forcear, 12.5)
-                              )}`
-                            : "None"}
-                    </td>
-                </tr>
-                <tr>
-                    <th>Force OD</th>
-                    <td>
-                        {params.forceod
-                            ? `OD: ${Math.max(0, Math.min(params.forceod, 11))}`
-                            : "None"}
-                    </td>
+                    <th>Forced Statistics</th>
+                    <td>{modifiedStats.join(" ") || "None"}</td>
                 </tr>
                 <tr>
                     <th>Estimated</th>
