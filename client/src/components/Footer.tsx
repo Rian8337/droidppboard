954970325b06dd663ca70c70042dfe302c974c88
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2024-01-07 14:03:31 +0800

    Bump copyright year

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <footer>
             <p>osu!droid Elaina PP Project Site</p>
-            <p>&copy; 2023 Rian8337</p>
+            <p>&copy; 2024 Rian8337</p>
             <DiscordServer />
         </footer>
     );
