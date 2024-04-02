449f42a2d8b0d555241767aac729aa989423584b
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2024-04-02 14:41:35 +0800

    Add "current work" to prototype description

--- client/src/components/PrototypeDescription.tsx
+++ client/src/components/PrototypeDescription.tsx
@@ -2,8 +2,20 @@ import "../styles/main.css";
 
 export default function PrototypeDescription() {
     return (
-        <div className="prototype-description">
-            The PP values in this list are under development!
-        </div>
+        <>
+            <div className="prototype-description">
+                The PP values in this list are under development!
+                <br />
+                <br />
+                Current work:{" "}
+                <a
+                    href="https://github.com/Rian8337/osu-droid-module/tree/yeahbennou-touch-sr"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    yeahbennou's touchscreen rework
+                </a>
+            </div>
+        </>
     );
 }
