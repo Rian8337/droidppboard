55110b68c4dbb537549b4e9c358be299a4cb71e1
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2024-04-02 14:38:24 +0800

    Update prototype pp structure

--- client/src/components/PlayItem.tsx
+++ client/src/components/PlayItem.tsx
@@ -23,9 +23,21 @@ const isTopPrototype = (play: Entry): play is TopPrototypePPEntry => {
 
 const getPPDescription = (play: Entry, index: number) => {
     // Would rather use a switch case here, but TypeScript being TypeScript
-    const weight: number = Math.pow(0.95, index);
+    const weight = Math.pow(0.95, index);
 
-    if (!isTop(play)) {
+    if (isTopPrototype(play) || isPrototype(play)) {
+        const diff = play.pp - play.prevPP;
+
+        let str = `(${diff >= 0 ? "+" : ""}${diff.toFixed(2)}pp`;
+
+        if (isPrototype(play) && !isTopPrototype(play)) {
+            str += `; ${(play.pp * weight).toFixed(2)}pp; weighted ${Math.round(
+                100 * weight
+            )}%`;
+        }
+
+        return str + ")";
+    } else if (!isTop(play)) {
         return `(${(play.pp * weight).toFixed(2)}pp; weighted ${Math.round(
             100 * weight
         )}%)`;
@@ -51,7 +63,15 @@ export default function PlayItem(props: { data: Entry; index: number }) {
                     ""
                 )}
                 <p className="play-info">
-                    {play.combo}x | {play.accuracy}% | <>{play.miss} ❌</>
+                    {play.combo}x | {play.accuracy}% |{" "}
+                    {isPrototype(play) && typeof play.hit300 === "number" ? (
+                        <>
+                            [{play.hit300}/{play.hit100}/{play.hit50}/
+                            {play.miss}]
+                        </>
+                    ) : (
+                        <>{play.miss} ❌</>
+                    )}
                     {play.speedMultiplier ? (
                         <>
                             {" "}
@@ -60,7 +80,56 @@ export default function PlayItem(props: { data: Entry; index: number }) {
                         </>
                     ) : null}
                 </p>
-
+                {isPrototype(play) ? (
+                    <>
+                        <p className="play-info">
+                            OD {play.overallDifficulty.toFixed(2)}
+                            {typeof play.calculatedUnstableRate === "number" ? (
+                                <>
+                                    {" "}
+                                    | {play.calculatedUnstableRate.toFixed(
+                                        2
+                                    )}{" "}
+                                    calculated UR
+                                </>
+                            ) : (
+                                <></>
+                            )}{" "}
+                            | {play.estimatedUnstableRate.toFixed(2)} estimated
+                            UR |{" "}
+                            {typeof play.estimatedSpeedUnstableRate === "number"
+                                ? play.estimatedSpeedUnstableRate.toFixed(2)
+                                : Infinity}{" "}
+                            estimated speed UR | {play.averageBPM.toFixed(2)}{" "}
+                            calculated average BPM |{" "}
+                            {play.speedNoteCount.toFixed(2)} speed note count
+                        </p>
+                        <p className="play-info">
+                            <b>Old</b>: {play.prevPP} pp ({play.prevAim} aim,{" "}
+                            {play.prevTap} tap, {play.prevAccuracy} accuracy,{" "}
+                            {play.prevVisual} visual)
+                        </p>
+                        <p className="play-info">
+                            <b>New</b>: {play.pp} pp ({play.newAim} aim,{" "}
+                            {play.newTap} tap, {play.newAccuracy} accuracy,{" "}
+                            {play.newVisual} visual)
+                        </p>
+                        <p className="play-info">
+                            Tap penalties:{" "}
+                            {(play.liveTapPenalty ?? 1).toFixed(2)} old,{" "}
+                            {(play.rebalanceTapPenalty ?? 1).toFixed(2)} new
+                        </p>
+                        <p className="play-info">
+                            Slider cheese penalties:{" "}
+                            {play.aimSliderCheesePenalty.toFixed(2)} aim,{" "}
+                            {play.flashlightSliderCheesePenalty.toFixed(2)}{" "}
+                            flashlight,{" "}
+                            {play.visualSliderCheesePenalty.toFixed(2)} visual
+                        </p>
+                    </>
+                ) : (
+                    <></>
+                )}
                 <div
                     className={
                         isPrototype(play) ? "prototype-mod-list" : "mod-list"
@@ -96,7 +165,9 @@ export default function PlayItem(props: { data: Entry; index: number }) {
                             : "pp-with-description"
                     }
                 >
-                    {play.pp}pp
+                    {isPrototype(play)
+                        ? `${play.prevPP}pp → ${play.pp}pp`
+                        : `${play.pp}pp`}
                 </p>
                 {description ? (
                     <p className="pp-description">{description}</p>
